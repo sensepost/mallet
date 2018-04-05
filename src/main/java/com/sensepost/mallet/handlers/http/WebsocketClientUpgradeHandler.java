7d08fe6e9d5b1cb489c33c69e3dbe653622f018c
Rogan Dawes <rogan@dawes.za.net>
2018-04-06 00:10:55 +0200

    Fix test for WebSocket Version requested
    It is typically specified in the REQUEST, not the response!

--- src/main/java/com/sensepost/mallet/handlers/http/WebsocketClientUpgradeHandler.java
+++ src/main/java/com/sensepost/mallet/handlers/http/WebsocketClientUpgradeHandler.java
@@ -3,21 +3,29 @@ package com.sensepost.mallet.handlers.http;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.websocketx.WebSocket00FrameDecoder;
+import io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder;
+import io.netty.handler.codec.http.websocketx.WebSocket07FrameDecoder;
+import io.netty.handler.codec.http.websocketx.WebSocket07FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocket08FrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocket08FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocket13FrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocket13FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocketFrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocketFrameEncoder;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 
 public class WebsocketClientUpgradeHandler extends ChannelDuplexHandler {
 
-	
+	private CharSequence reqVersion = null;
+
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
@@ -29,21 +37,38 @@ public class WebsocketClientUpgradeHandler extends ChannelDuplexHandler {
 						HttpHeaderNames.SEC_WEBSOCKET_VERSION);
 				final WebSocketFrameDecoder decoder;
 				final WebSocketFrameEncoder encoder;
-				if ("13".equals(version)) {
-					decoder = new WebSocket13FrameDecoder(false, true,
-							Integer.MAX_VALUE, true);
-					encoder = new WebSocket13FrameEncoder(true);
-				} else if ("08".equals(version)) {
-					decoder = new WebSocket08FrameDecoder(false, true,
-							Integer.MAX_VALUE, true);
-					encoder = new WebSocket08FrameEncoder(true);
-				} else { // FIXME: handle the other options
-					decoder = null;
-					encoder = null;
-				}
+				
+				if (version == null)
+					version = reqVersion;
+				
+		        if (version != null) {
+		            if (version.equals(WebSocketVersion.V13.toHttpHeaderValue())) {
+		                // Version 13 of the wire protocol - RFC 6455 (version 17 of the draft hybi specification).
+						decoder = new WebSocket13FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket13FrameEncoder(true);
+		            } else if (version.equals(WebSocketVersion.V08.toHttpHeaderValue())) {
+		                // Version 8 of the wire protocol - version 10 of the draft hybi specification.
+						decoder = new WebSocket08FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket08FrameEncoder(true);
+		            } else if (version.equals(WebSocketVersion.V07.toHttpHeaderValue())) {
+		                // Version 8 of the wire protocol - version 07 of the draft hybi specification.
+						decoder = new WebSocket07FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket07FrameEncoder(true);
+		            } else {
+		            	decoder = null;
+		            	encoder = null;
+		            }
+		        } else {
+		            // Assume version 00 where version header was not specified
+					decoder = new WebSocket00FrameDecoder(Integer.MAX_VALUE);
+					encoder = new WebSocket00FrameEncoder();
+		        }
+
 
 				ChannelPipeline p = ctx.pipeline();
-				System.out.println("Pipeline is : " + p);
 				ChannelHandlerContext httpCtx = p.context(HttpClientCodec.class);
 				HttpClientCodec http = (HttpClientCodec) httpCtx.handler();
 				ChannelHandlerContext aggCtx = p.context(HttpObjectAggregator.class);
@@ -58,11 +83,26 @@ public class WebsocketClientUpgradeHandler extends ChannelDuplexHandler {
 					ctx.pipeline().addAfter(ctx.name(), null,
 							encoder);
 				}
-				System.out.println("Pipeline is now : " + p);
 				ctx.pipeline().remove(this);
+				ctx.fireUserEventTriggered("Removed WebSocketClientUpgradeHandler");
+				if (encoder != null) 
+					ctx.fireUserEventTriggered("WebSocket Encoder class is " + encoder.getClass().getSimpleName());
 			}
 		}
 		super.channelRead(ctx, msg);
 	}
 
+
+	@Override
+	public void write(ChannelHandlerContext ctx, Object msg,
+			ChannelPromise promise) throws Exception {
+		if (msg instanceof HttpRequest) {
+			HttpRequest req = (HttpRequest) msg;
+			reqVersion = req.headers().get(
+					HttpHeaderNames.SEC_WEBSOCKET_VERSION);
+		}
+		super.write(ctx, msg, promise);
+	}
+
+	
 }
--- src/main/java/com/sensepost/mallet/handlers/http/WebsocketServerUpgradeHandler.java
+++ src/main/java/com/sensepost/mallet/handlers/http/WebsocketServerUpgradeHandler.java
@@ -9,16 +9,35 @@ import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocket00FrameDecoder;
+import io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder;
+import io.netty.handler.codec.http.websocketx.WebSocket07FrameDecoder;
+import io.netty.handler.codec.http.websocketx.WebSocket07FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocket08FrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocket08FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocket13FrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocket13FrameEncoder;
 import io.netty.handler.codec.http.websocketx.WebSocketFrameDecoder;
 import io.netty.handler.codec.http.websocketx.WebSocketFrameEncoder;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 
 public class WebsocketServerUpgradeHandler extends ChannelDuplexHandler {
+
+	private CharSequence reqVersion = null;
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		if (msg instanceof HttpRequest) {
+			HttpRequest req = (HttpRequest) msg;
+			reqVersion = req.headers().get(
+					HttpHeaderNames.SEC_WEBSOCKET_VERSION);
+		}
+		super.channelRead(ctx, msg);
+	}
 
 	@Override
 	public void write(final ChannelHandlerContext ctx, Object msg,
@@ -31,19 +50,38 @@ public class WebsocketServerUpgradeHandler extends ChannelDuplexHandler {
 						HttpHeaderNames.SEC_WEBSOCKET_VERSION);
 				final WebSocketFrameDecoder decoder;
 				final WebSocketFrameEncoder encoder;
-				if ("13".equals(version)) {
-					decoder = new WebSocket13FrameDecoder(false, true,
-							Integer.MAX_VALUE, true);
-					encoder = new WebSocket13FrameEncoder(true);
-				} else if ("08".equals(version)) {
-					decoder = new WebSocket08FrameDecoder(false, true,
-							Integer.MAX_VALUE, true);
-					encoder = new WebSocket08FrameEncoder(true);
-				} else { // FIXME: handle the other options
-					decoder = null;
-					encoder = null;
-				}
+				
+				if (version == null)
+					version = reqVersion;
+				
+		        if (version != null) {
+		            if (version.equals(WebSocketVersion.V13.toHttpHeaderValue())) {
+		                // Version 13 of the wire protocol - RFC 6455 (version 17 of the draft hybi specification).
+						decoder = new WebSocket13FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket13FrameEncoder(true);
+		            } else if (version.equals(WebSocketVersion.V08.toHttpHeaderValue())) {
+		                // Version 8 of the wire protocol - version 10 of the draft hybi specification.
+						decoder = new WebSocket08FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket08FrameEncoder(true);
+		            } else if (version.equals(WebSocketVersion.V07.toHttpHeaderValue())) {
+		                // Version 8 of the wire protocol - version 07 of the draft hybi specification.
+						decoder = new WebSocket07FrameDecoder(false, true,
+								Integer.MAX_VALUE, true);
+						encoder = new WebSocket07FrameEncoder(true);
+		            } else {
+		            	decoder = null;
+		            	encoder = null;
+		            }
+		        } else {
+		            // Assume version 00 where version header was not specified
+					decoder = new WebSocket00FrameDecoder(Integer.MAX_VALUE);
+					encoder = new WebSocket00FrameEncoder();
+		        }
 
+		        // if the upgrade was successful, we HAVE to remove the HTTP codecs
+		        // even if we didn't identify the correct websocket version
 				promise.addListener(new ChannelFutureListener() {
 
 					@Override
@@ -62,6 +100,9 @@ public class WebsocketServerUpgradeHandler extends ChannelDuplexHandler {
 										encoder);
 							}
 							ctx.pipeline().remove(WebsocketServerUpgradeHandler.this);
+							ctx.fireUserEventTriggered("Removed WebSocketServerUpgradeHandler");
+							if (encoder != null) 
+								ctx.fireUserEventTriggered("WebSocket Encoder class is " + encoder.getClass().getSimpleName());
 						}
 					}
 				});
