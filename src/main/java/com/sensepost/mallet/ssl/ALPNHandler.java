78347ecdfb7f392a37abd09dbbabfbd0bf90fa91
Rogan Dawes <rogan@dawes.za.net>
2019-02-06 20:49:37 +0200

    Clean up unnecessary code

--- src/main/java/com/sensepost/mallet/ssl/ALPNHandler.java
+++ src/main/java/com/sensepost/mallet/ssl/ALPNHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
 import io.netty.handler.ssl.ApplicationProtocolNames;
 import io.netty.handler.ssl.ApplicationProtocolNegotiationHandler;
 
@@ -42,34 +43,12 @@ public class ALPNHandler extends ApplicationProtocolNegotiationHandler
 	}
 
 	@Override
-	public void optionSelected(ChannelHandlerContext ctx, String option)
-			throws Exception {
+	public void optionSelected(ChannelHandlerContext ctx, String option) throws Exception {
 		GraphLookup gl = ctx.channel().attr(ChannelAttributes.GRAPH).get();
 		if (gl == null)
 			throw new NullPointerException("gl");
-		ChannelHandler[] handlers = gl.getNextHandlers(this, option);
-		String name = ctx.name();
-		List<String> names = ctx.pipeline().names();
-		int pos = names.indexOf(name);
-		ChannelHandler exceptionCatcher = null;
-		if (pos > -1 && pos < names.size() - 1) {
-			String next = names.get(pos + 1);
-			ChannelHandler nextHandler = ctx.pipeline().get(next);
-			if (nextHandler instanceof ExceptionCatcher)
-				exceptionCatcher = nextHandler;
-		}
-		try {
-			ctx.pipeline().addLast(handlers);
-		} catch (Exception e) {
-			ctx.fireExceptionCaught(e);
-			Channel ch = ctx.channel().attr(ChannelAttributes.CHANNEL)
-					.get();
-			ctx.close();
-			if (ch != null && ch.isOpen())
-				ch.close();
-		}
-		if (exceptionCatcher != null)
-			ctx.pipeline().remove(exceptionCatcher);
+		ChannelInitializer<Channel> initializer = gl.getNextHandlers(this, option);
+		ctx.pipeline().replace(this, null, initializer);
 	}
 
 }
