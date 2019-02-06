2d53fdae2ec229eded60830f32e69dda3adb961d
Rogan Dawes <rogan@dawes.za.net>
2019-02-06 21:13:56 +0200

    Introduce a SubChannel, and SubChannelHandler
    The purpose of the SubChannel is to provide a "self-contained" Channel
    for user handlers to be added to, such that there will always remain an
    internal handler at the end of the pipeline.
    
    This is achieved by directing incoming events on the primary channel
    into the SubChannel via the SubChannelHandler, through the user
    handlers, then potentially back into the primary channel. The main
    advantage of this scheme is to ensure that events will be logged even
    when the pipeline contains IndeterminateChannelHandlers, where the
    InterceptHandler is only added to the pipeline AFTER events like the
    ConnectTarget user event have been fired.
    
    In the normal scheme of things, if a RelayHandler is added to the
    SubChannel, no events should reach the end of the primary channel,
    because RelayHandler should only forward appropriate events into the
    other SubChannel, and not pass them further up its own pipeline.

---
+++ src/main/java/com/sensepost/mallet/channel/SubChannelHandler.java
@@ -0,0 +1,147 @@
+package com.sensepost.mallet.channel;
+
+import java.net.SocketAddress;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.socket.DatagramChannel;
+import io.netty.channel.socket.DuplexChannel;
+
+public class SubChannelHandler extends ChannelDuplexHandler {
+
+	private ChannelHandler[] handlers;
+	private SubChannel sc;
+
+	public SubChannelHandler(ChannelHandler... handlers) {
+		this.handlers = handlers;
+	}
+
+	public Channel subChannel() {
+		return sc;
+	}
+
+	@Override
+	public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+		sc.bind(localAddress).addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
+			ChannelPromise promise) throws Exception {
+		sc.connect(remoteAddress, localAddress).addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		sc.disconnect().addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		sc.close().addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		sc.deregister().addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void read(ChannelHandlerContext ctx) throws Exception {
+		sc.pipeline().read();
+	}
+
+	@Override
+	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+		sc.write(msg).addListener(new PromiseRelay(promise));
+	}
+
+	@Override
+	public void flush(ChannelHandlerContext ctx) throws Exception {
+		sc.flush();
+	}
+
+	@Override
+	public void channelRegistered(final ChannelHandlerContext ctx) throws Exception {
+		ChannelPromise promise = ctx.channel().newPromise();
+//		promise.addListener(new ChannelFutureListener() {
+//			@Override
+//			public void operationComplete(ChannelFuture future) throws Exception {
+//				ctx.fireChannelRegistered();
+//			}
+//		});
+		sc.unsafe().register(ctx.channel().eventLoop(), promise);
+	}
+
+	@Override
+	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+		sc.deregister();
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		sc.pipeline().fireChannelActive();
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		sc.pipeline().fireChannelInactive();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		sc.pipeline().fireChannelRead(msg);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		sc.pipeline().fireChannelReadComplete();
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		sc.pipeline().fireUserEventTriggered(evt);
+	}
+
+	@Override
+	public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+		sc.pipeline().fireChannelWritabilityChanged();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		sc.pipeline().fireExceptionCaught(cause);
+	}
+
+	@Override
+	public boolean isSharable() {
+		return false;
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		if (ctx.channel() instanceof DatagramChannel)
+			sc = new DatagramSubChannel(ctx);
+		else if (ctx.channel() instanceof DuplexChannel)
+			sc = new DuplexSubChannel(ctx);
+		else 
+			sc = new SubChannel(ctx);
+		if (handlers != null)
+			sc.pipeline().addFirst(handlers);
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		ChannelHandler first;
+		while ((first = sc.pipeline().first()) != null) {
+			sc.pipeline().remove(first);
+		}
+	}
+
+}
