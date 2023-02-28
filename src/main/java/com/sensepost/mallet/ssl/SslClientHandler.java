ae7a9106804d38a6d954d1b827de7114a87019e6
Rogan Dawes <rogan@dawes.za.net>
2023-02-28 12:42:16 +0200

    Add warnings about SNI when only an IP address is provided.
    Only if the server throws an exception while negotiating
    the connection.

--- src/main/java/com/sensepost/mallet/ssl/SslClientHandler.java
+++ src/main/java/com/sensepost/mallet/ssl/SslClientHandler.java
@@ -1,5 +1,13 @@
 package com.sensepost.mallet.ssl;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.security.Security;
+import java.security.cert.Certificate;
+
+import javax.net.ssl.X509KeyManager;
+
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPipeline;
@@ -8,58 +16,86 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.security.Security;
-
-import javax.net.ssl.X509KeyManager;
+import io.netty.util.NetUtil;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
 
 public class SslClientHandler extends ChannelOutboundHandlerAdapter {
 
-	private SslContextBuilder builder = null;
-	
-	public SslClientHandler() {
-		this(null, null, null);
-	}
+    private SslContextBuilder builder = null;
 
-	public SslClientHandler(X509KeyManager km, String alias) {
-		this(null, km, alias);
-	}
+    public SslClientHandler() {
+        this(null, null, null);
+    }
 
-	public SslClientHandler(String provider, X509KeyManager km, String alias) {
-		builder = SslContextBuilder.forClient().trustManager(
-				InsecureTrustManagerFactory.INSTANCE);
-		if (provider != null)
-			builder.sslContextProvider(Security.getProvider(provider));
-		if (km != null && alias != null)
-			builder.keyManager(km.getPrivateKey(alias),
-					km.getCertificateChain(alias));
-		builder.protocols(new String[] { "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"});
-	}
+    public SslClientHandler(X509KeyManager km, String alias) {
+        this(null, km, alias);
+    }
 
-	public SslClientHandler(SslContextBuilder builder) {
-		this.builder = builder;
-	}
-	
-	@Override
-	public void connect(ChannelHandlerContext ctx, final SocketAddress remoteAddress,
-			SocketAddress localAddress, ChannelPromise promise)
-			throws Exception {
-		ChannelPipeline p = ctx.channel().pipeline();
-		String me = ctx.name();
-		SslContext clientContext = builder.build();
-		final SslHandler s;
-		if (remoteAddress instanceof InetSocketAddress) {
-			InetSocketAddress remote = (InetSocketAddress) remoteAddress;
-			s = clientContext.newHandler(ctx.alloc(), remote.getHostString(), remote.getPort());
-		} else {
-			s = clientContext.newHandler(ctx.alloc());
-		}
+    public SslClientHandler(String provider, X509KeyManager km, String alias) {
+        builder = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE);
+        if (provider != null)
+            builder.sslContextProvider(Security.getProvider(provider));
+        if (km != null && alias != null)
+            builder.keyManager(km.getPrivateKey(alias), km.getCertificateChain(alias));
+        builder.protocols(new String[] { /* "SSLv3", */ "TLSv1", "TLSv1.1", "TLSv1.2" });
+    }
 
-		p.addAfter(me, null, s);
-		p.remove(this);
-		super.connect(ctx, remoteAddress, localAddress, promise);
-	}
+    public SslClientHandler(SslContextBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public void connect(ChannelHandlerContext ctx, final SocketAddress remoteAddress, SocketAddress localAddress,
+            ChannelPromise promise) throws Exception {
+        replaceSslHandler(ctx, remoteAddress).connect(ctx, remoteAddress, localAddress, promise);
+    }
+
+    @Override
+    public void read(ChannelHandlerContext ctx) throws Exception {
+        replaceSslHandler(ctx, ctx.channel().remoteAddress()).read(ctx);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        replaceSslHandler(ctx, ctx.channel().remoteAddress()).write(ctx, msg, promise);
+    }
+
+    protected SslHandler replaceSslHandler(ChannelHandlerContext ctx, final SocketAddress remoteAddress)
+            throws Exception {
+        final ChannelPipeline p = ctx.channel().pipeline();
+        String me = ctx.name();
+        SslContext clientContext = builder.build();
+        final SslHandler s;
+        if (remoteAddress instanceof InetSocketAddress) {
+            InetSocketAddress remote = (InetSocketAddress) remoteAddress;
+            s = clientContext.newHandler(ctx.alloc(), remote.getHostString(), remote.getPort());
+            ctx.fireUserEventTriggered("Adding SslClientHandler for " + remote.getHostString());
+        } else {
+            s = clientContext.newHandler(ctx.alloc());
+            ctx.fireUserEventTriggered("Adding SslClientHandler for default client");
+        }
+        s.handshakeFuture().addListener(new FutureListener<Channel>() {
+            @Override
+            public void operationComplete(Future<Channel> future) throws Exception {
+                if (future.isSuccess()) {
+                    Certificate[] serverCerts = s.engine().getSession().getPeerCertificates();
+                    ctx.fireUserEventTriggered(serverCerts);
+                } else {
+                    if (remoteAddress instanceof InetSocketAddress) {
+                        InetSocketAddress remote = (InetSocketAddress) remoteAddress;
+                        if (NetUtil.isValidIpV4Address(remote.getHostString())
+                                || NetUtil.isValidIpV6Address(remote.getHostString())) {
+                            ctx.fireUserEventTriggered("HandshakeException connecting to IP address "
+                                    + remote.getHostString() + ". Remote end may be expecting a hostname via SNI.\n"
+                                    + "Make sure that you have enabled remote DNS lookup (SOCKS4a or SOCKS5h) or similar.");
+                        }
+                    }
+                }
+            }
+        });
+        p.replace(me, null, s);
+        return s;
+    }
 
 }
