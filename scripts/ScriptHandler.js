eeeccfc66365fb56bb1fc31932e4fa86ba207539
Rogan Dawes <rogan@dawes.za.net>
2024-06-18 17:16:12 +0200

    Improve JavaScript example slightly

--- scripts/ScriptHandler.js
+++ scripts/ScriptHandler.js
@@ -1,8 +1,12 @@
 var ChannelDuplexHandler = Java.type("io.netty.channel.ChannelDuplexHandler");
 var MyChannelDuplexHandler = Java.extend(ChannelDuplexHandler, {
     channelRead: function(ctx, msg) {
-        ctx.fireUserEventTriggered("Hello from Javascript");
+        ctx.fireUserEventTriggered("Read from Javascript");
         ctx.fireChannelRead(msg);
+    },
+    write: function(ctx, msg, promise) {
+        ctx.fireUserEventTriggered("Write from Javascript");
+        ctx.write(msg, promise);
     }
 });
 var _ = new MyChannelDuplexHandler();
