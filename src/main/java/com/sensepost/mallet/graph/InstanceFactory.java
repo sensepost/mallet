21d6e4de2d3fc9caadaee7ae8978a1ec303bcedc
Rogan Dawes <rogan@dawes.za.net>
2024-06-20 14:27:03 +0200

    Try to sort constructors to avoid String parameters first
    The problem comes when a parameter could be coerced from a String to
    a more specific type (e.g. by resolving it to a static field in a class)
    but the constructor can also accept a String parameter, and coercing a
    String to a String takes no doing.

--- src/main/java/com/sensepost/mallet/graph/InstanceFactory.java
+++ src/main/java/com/sensepost/mallet/graph/InstanceFactory.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Parameter;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Arrays;
+import java.util.Comparator;
 
 import javax.script.Bindings;
 
@@ -14,10 +15,55 @@ public class InstanceFactory {
 
 	private Bindings scriptContext;
 
+	/**
+	 * Sorter intended to reduce the chance that a parameter string is used as is
+	 * when it could be resolved to something more specific that would match
+	 * the constructor. i.e. prefer constructors with non-String parameters
+	 */
+	Comparator<Constructor<?>> stringLastSorter = new Comparator<Constructor<?>>() {
+
+        @Override
+        public int compare(Constructor<?> o1, Constructor<?> o2) {
+            Parameter[] p1 = o1.getParameters();
+            Parameter[] p2 = o2.getParameters();
+            int s1 = countStringParameters(p1);
+            int s2 = countStringParameters(p2);
+            if (s1 < s2)
+                return -1;
+            else if (s2 < s1)
+                return 1;
+            return 0;
+        }
+    };
+
+    private int countStringParameters(Parameter[] p) {
+        int c = 0;
+        for (int i=0; i<p.length; i++) {
+            if (p[i].getType().isAssignableFrom(String.class))
+                c++;
+        }
+        return c;
+    }
+
 	public InstanceFactory(Bindings scriptContext) {
 		this.scriptContext = scriptContext;
 	}
 
+	/** 
+	 * Attempts to instantiate an object of the specified type, based on
+	 * the classname provided in the description, using the String
+	 * of arguments. An attempt is made to coerce the string arguments
+	 * to the required type to match those accepted by the type Constructors.
+	 * 
+	 * @param description The desired class name, or else a static field
+	 * in a class
+	 * @param type the desired object should be derived from this class.
+	 * @param arguments Any arguments required by the constructor.
+	 * @return an instance of the described object
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
 	public Object getClassInstance(String description, Class<?> type,
 			String[] arguments) throws ClassNotFoundException,
 			InstantiationException, IllegalAccessException {
@@ -29,23 +75,24 @@ public class InstanceFactory {
 		if (description.startsWith("{")) {
 			return getInternalInstance(description, type, arguments);
 		}
-		// try to return a static field of a class
-		try {
-			int dot = description.lastIndexOf('.');
-			if (dot > 0) {
-				String clsname = description.substring(0, dot);
-				String fieldName = description.substring(dot + 1);
-				Class<?> clz = Class.forName(clsname);
-				Field f = clz.getField(fieldName);
-				if (Modifier.isStatic(f.getModifiers())) {
-					Object instance = f.get(null);
-					if (instance != null
-							&& type.isAssignableFrom(instance.getClass()))
-						return instance;
-				}
-			}
-		} catch (ClassNotFoundException | NoSuchFieldException e) {
-			// that didn't work, try something else
+		if (arguments.length == 0) { // try to return a static field of a class
+    		try {
+    			int dot = description.lastIndexOf('.');
+    			if (dot > 0) {
+    				String clsname = description.substring(0, dot);
+    				String fieldName = description.substring(dot + 1);
+    				Class<?> clz = Class.forName(clsname);
+    				Field f = clz.getField(fieldName);
+    				if (Modifier.isStatic(f.getModifiers())) {
+    					Object instance = f.get(null);
+    					if (instance != null
+    							&& type.isAssignableFrom(instance.getClass()))
+    						return instance;
+    				}
+    			}
+    		} catch (ClassNotFoundException | NoSuchFieldException e) {
+    			// that didn't work, try something else
+    		}
 		}
 		// See if it is a basic type that can easily be converted from a String
 		if (type.equals(String.class)) {
@@ -72,6 +119,7 @@ public class InstanceFactory {
 			Class<?> clz = Class.forName(description);
 			if (type.isAssignableFrom(clz)) {
 				Constructor<?>[] constructors = clz.getConstructors();
+				Arrays.sort(constructors, stringLastSorter);
 				for (Constructor<?> c : constructors) {
 					log.append("Considering " + c + "\n");
 					Object[] args = null;
